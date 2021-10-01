65a6a95c8faa19f42d587a9da22116265004776a
Stefanos Missin <s.missin@zonepage.gr>
2021-10-01 11:32:33 +0300

    first commit

---
+++ resources/views/zp.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AKOU API - BY ZONEPAGE</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+</head>
+<body class="bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <img src="/assets/images/zp-logo.png" width="800">
+            </div>
+        </div>
+     
+    </div>
+</body>
+</html>
